2a9a5db7af2222ced555d4f2b5abfc062fbf296a
cybergartar <kamonnop.a@gmail.com>
2016-01-23 22:08:15 +0700

    Tst Branch

--- MainScreen.java
+++ MainScreen.java
@@ -47,7 +47,7 @@ public class MainScreen extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main_screen, menu);
         return true;
     }
