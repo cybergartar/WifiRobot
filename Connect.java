41f8fcef60eea4def88bc09da74411f033a28341
cybergartar <kamonnop.a@gmail.com>
2016-01-26 16:06:36 +0700

    EieiGum

--- Connect.java
+++ Connect.java
@@ -4,9 +4,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.ExtractedTextRequest;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,18 +13,13 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Scanner;
 
 
 /**
@@ -49,14 +42,85 @@ public class Connect extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        Button upBtn = (Button)findViewById(R.id.upBtn);
+        Button downBtn = (Button)findViewById(R.id.downBtn);
+        Button leftBtn = (Button)findViewById(R.id.leftBtn);
+        Button rightBtn = (Button)findViewById(R.id.rightBtn);
+        upBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new httpConnection().execute("http://128.199.158.8:3000/connect/up");
+            }
+        });
+        downBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new httpConnection().execute("http://128.199.158.8:3000/connect/down");
+            }
+        });
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new httpConnection().execute("http://128.199.158.8:3000/connect/left");
+            }
+        });
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new httpConnection().execute("http://128.199.158.8:3000/connect/right");
+            }
+        });
 
     }
 
     protected class httpConnection extends AsyncTask<String, Integer, String>{
         @Override
-        protected String doInBackground(String... params) {
-            String result = "";
+        protected String doInBackground(String... urls) {
+            String parsedString="";
+            try {
 
+                URL url = new URL(urls[0]);
+                URLConnection conn = url.openConnection();
+
+                HttpURLConnection httpConn = (HttpURLConnection) conn;
+                httpConn.setAllowUserInteraction(false);
+                httpConn.setInstanceFollowRedirects(true);
+                httpConn.setRequestMethod("GET");
+                httpConn.connect();
+
+                InputStream is = httpConn.getInputStream();
+                parsedString = convertinputStreamToString(is);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return parsedString;
+        }
+        public String convertinputStreamToString(InputStream ists)
+                throws IOException {
+            if (ists != null) {
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                try {
+                    BufferedReader r1 = new BufferedReader(new InputStreamReader(
+                            ists, "UTF-8"));
+                    while ((line = r1.readLine()) != null) {
+                        sb.append(line).append("\n");
+                    }
+                } finally {
+                    ists.close();
+                }
+                return sb.toString();
+            } else {
+                return "";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String parsedString) {
+            TextView opt = (TextView)findViewById(R.id.responseTest);
+            opt.setText(parsedString);
         }
     }
 
